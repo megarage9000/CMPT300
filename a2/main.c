d0872cbc64a645ba2c9091f5fe835a080e2efc47
John Ordoyo <gtom_1984@hotmail.ca>
2021-06-23 15:24:07 -0700

    Work with threading and stuff ;)
    - Need to develop consumer / producer monitor system for threads

--- a2/main.c
+++ a2/main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
-    
+    char buf[100];
+    char printMessage[] = "Enter a message\n";
+    while(1) {
+        while (read(STDIN_FILENO, buf, sizeof buf) > 0) {
+            write(STDOUT_FILENO, printMessage, sizeof printMessage);
+            buf[100] = '\0';
+            printf("You entered = %s\n", buf);
+        }
+    }
 }
